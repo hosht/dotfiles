21d02035ecf11b8d8f4d7c714a0cd5c38507e562
Takanobu Hoshino <hoshino@hey.com>
2025-10-10 12:33:37 +0900

    Remove functions dependent on xsv

--- .zsh/rc/function.zsh
+++ .zsh/rc/function.zsh
@@ -5,21 +5,3 @@
 function ghqfzf() {
     anyframe-widget-cd-ghq-repository
 }
-
-function ghpr() {
-    gh pr list \
-        | xsv table \
-        | anyframe-selector-auto \
-        | awk '{print $(NF-5)}' \
-        | anyframe-action-execute git switch
-}
-
-# gcloud cli
-function gcactivate() {
-     gcloud config configurations list \
-        | tail -n +2 \
-        | xsv table \
-        | anyframe-selector-auto \
-        | awk '{print $1}' \
-        | anyframe-action-execute gcloud config configurations activate
-}
